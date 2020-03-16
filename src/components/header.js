792b5a0b521d1e11ee069e00ae3634387cacaec9
camunoz2 <camunoz2@gmail.com>
2020-03-16 16:57:12 -0300

    logo insert

---
+++ src/components/header.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Logo from "../assets/logo_lnsf.png"
+
+export default () => (
+    <div className="container mx-auto">
+        <img className="inline-block h-48" src={Logo} alt="Logo LNSF"/>
+    </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
+import Header from "../components/header"
 
 export default () => (
-    <div className="container mx-auto">
-        <h1>Hello!!</h1>
-    </div>
+    <Header></Header>
 )
