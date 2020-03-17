dddfc3c25e48ae4c3faa8123a59277f753153847
camunoz2 <camunoz2@gmail.com>
2020-03-17 09:24:19 -0300

    installed open sans, fontawesome icons

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,12 @@
 import React from "react"
-import Logo from "../assets/logo_lnsf.png"
+import Logo from "../assets/lnsf_insignia.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faReact } from "@fortawesome/free-brands-svg-icons"
+
 
 export default () => (
     <div className="container mx-auto">
         <img className="inline-block h-48" src={Logo} alt="Logo LNSF"/>
+        <FontAwesomeIcon icon={faReact}/>
     </div>
 )
